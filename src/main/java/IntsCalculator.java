f5d0b32c7f33cc19c032918da6e43c3a8e0fcc85
Alex <3696323@gmail.com>
2022-05-21 21:09:44 +0300

    new

--- src/main/java/IntsCalculator.java
+++ src/main/java/IntsCalculator.java
@@ -1,27 +1,29 @@
 public class IntsCalculator implements Ints {
     protected final Calculator target;
 
-    public IntsCalculator() { this.target = new Calculator(); }
+    public IntsCalculator() {
+        this.target = new Calculator();
+    }
 
     @Override
     public int sum(int arg0, int arg1) {
         //считаем через target
 
-        double res= target.newFormula().addOperand(arg0).addOperand(arg1).calculate(Calculator.Operation.SUM).result;
+        double res = target.newFormula().addOperand(arg0).addOperand(arg1).calculate(Calculator.Operation.SUM).result;
         return (int) res;
     }
 
     @Override
     public int mult(int arg0, int arg1) {
         //считаем через target
-        double res= target.newFormula().addOperand(arg0).addOperand(arg1).calculate(Calculator.Operation.MULT).result;
+        double res = target.newFormula().addOperand(arg0).addOperand(arg1).calculate(Calculator.Operation.MULT).result;
         return (int) res;
     }
 
     @Override
     public int pow(int a, int b) {
         //считаем через target
-        double res= target.newFormula().addOperand(a).addOperand(b).calculate(Calculator.Operation.POW).result;
+        double res = target.newFormula().addOperand(a).addOperand(b).calculate(Calculator.Operation.POW).result;
         return (int) res;
     }
 }
